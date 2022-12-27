4160f46ebdf54d61f6a842b1486c7647fb7191ea
abdulkareemm <eng.kreem.96@gmail.com>
2022-12-27 00:59:58 -0800

    chsnge url

--- astar/urls.py
+++ astar/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/',views.say_hello)
+    path('/solve',views.say_hello)
 ]
--- reemhomework/urls.py
+++ reemhomework/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('solve/',include('astar.urls')),
+    #path('admin/', admin.site.urls),
+    path('reem/',include('astar.urls')),
 ]
